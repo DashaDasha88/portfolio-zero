386594e20399f7010dd617ccae58b6eee838f7f4
Dasha <dasha.kotova.mail@gmail.com>
2021-06-25 16:46:03 -0400

    add hero component

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,19 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        This is my value proposition. This is what I can do and help you with.
+      </SectionText>
+      <Button onClick={() => window.location = "https://google.com"}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
