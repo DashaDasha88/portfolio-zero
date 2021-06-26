76696211c1ead3d706630cf5821fd981d0aca0cc
Dasha <dasha.kotova.mail@gmail.com>
2021-06-26 17:53:10 -0400

    modify skills and technologies component

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,26 +1,5 @@
 import React from 'react';
-import { 
-  DiBootstrap,
-  DiGit, 
-  DiGithub, 
-  DiGoogleAnalytics, 
-  DiHtml5, 
-  DiJavascript1, 
-  DiJira, 
-  DiJqueryLogo, 
-  DiLinux, 
-  DiMailchimp,
-  DiNodejs,
-  DiNpm,
-  DiPhotoshop,
-  DiPostgresql,
-  DiReact, 
-  DiSass,
-  DiScrum,
-  DiTrello,
-  DiUbuntu,
-  DiWordpress
-} from 'react-icons/di';
+import { DiFirebase, DiReact, DiTrello } from 'react-icons/di';
 import {
   SiMaterialUi
 } from 'react-icons/si'
@@ -30,208 +9,56 @@ import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './Techn
 const Technologies = () =>  (
   <Section id="tech">
     <SectionDivider />
-    <SectionTitle>Technologies</SectionTitle>
+    <br />
+    <SectionTitle>Skills & Technologies</SectionTitle>
     <SectionText>
-      Here's all the tech that I know.
+      Here are all the cool technologies that I know.
     </SectionText>
     <List>
 
       <ListItem>
-        <DiBootstrap size="3rem" />
-        <ListContainer>
-          <ListParagraph>
-            Bootstrap
-          </ListParagraph>
-        </ListContainer>
+        <ListTitle>Column One</ListTitle>
+        <DiFirebase size="3rem" />
+          <ListContainer>
+            <ListParagraph>
+              One
+              <br />
+              Two
+              <br />
+              Three
+              <br />
+            </ListParagraph>
+          </ListContainer>
       </ListItem>
 
       <ListItem>
-        <DiGit size="3rem" />
-        <ListContainer>
-          <ListParagraph>
-            Git
-          </ListParagraph>
-        </ListContainer>
+        <ListTitle>Column Two</ListTitle>
+        <DiFirebase size="3rem" />
+          <ListContainer>
+            <ListParagraph>
+              One
+              <br />
+              Two
+              <br />
+              Three
+              <br />
+            </ListParagraph>
+          </ListContainer>
       </ListItem>
 
       <ListItem>
-        <DiGithub size="3rem" />
-        <ListContainer>
-          <ListParagraph>
-            GitHub
-          </ListParagraph>
-        </ListContainer>
-      </ListItem>
-
-      <ListItem>
-        <DiGoogleAnalytics size="3rem" />
-        <ListContainer>
-          <ListParagraph>
-            Google Analytics
-          </ListParagraph>
-        </ListContainer>
-      </ListItem>
-
-      <ListItem>
-        <DiHtml5 size="3rem" />
-        <ListContainer>
-          <ListParagraph>
-            HTML
-          </ListParagraph>
-        </ListContainer>
-      </ListItem>
-
-      <ListItem>
-        <DiJavascript1 size="3rem" />
-        <ListContainer>
-          <ListParagraph>
-            JavaScript
-          </ListParagraph>
-        </ListContainer>
-      </ListItem>
-
-      <ListItem>
-        <DiJira size="3rem" />
-        <ListContainer>
-          <ListParagraph>
-            Jira
-          </ListParagraph>
-        </ListContainer>
-      </ListItem>
-
-      <ListItem>
-        <DiJqueryLogo size="3rem" />
-        <ListContainer>
-          <ListParagraph>
-            jQuery
-          </ListParagraph>
-        </ListContainer>
-      </ListItem>
-
-      <ListItem>
-        <DiLinux size="3rem" />
-        <ListContainer>
-          <ListParagraph>
-            Linux
-          </ListParagraph>
-        </ListContainer>
-      </ListItem>
-
-      <ListItem>
-        <DiMailchimp size="3rem" />
-        <ListContainer>
-          <ListParagraph>
-            Mailchimp
-          </ListParagraph>
-        </ListContainer>
-      </ListItem>
-{/* 
-      <ListItem>
-        <SiMaterialUI size="3rem" />
-        <ListContainer>
-          <ListParagraph>
-            MaterialUi
-          </ListParagraph>
-        </ListContainer>
-      </ListItem> */}
-
-      <ListItem>
-        <DiNodejs size="3rem" />
-        <ListContainer>
-          <ListParagraph>
-            Node.js
-          </ListParagraph>
-        </ListContainer>
-      </ListItem>
-
-      <ListItem>
-        <DiNpm size="3rem" />
-        <ListContainer>
-          <ListParagraph>
-            npm
-          </ListParagraph>
-        </ListContainer>
-      </ListItem>
-
-      <ListItem>
-        <DiPhotoshop size="3rem" />
-        <ListContainer>
-          <ListParagraph>
-            Photoshop
-          </ListParagraph>
-        </ListContainer>
-      </ListItem>
-
-      <ListItem>
-        <DiPostgresql size="3rem" />
-        <ListContainer>
-          <ListParagraph>
-            PostgreSQL
-          </ListParagraph>
-        </ListContainer>
-      </ListItem>
-
-      <ListItem>
-        <DiReact size="3rem" />
-        <ListContainer>
-          <ListParagraph>
-            React.js
-          </ListParagraph>
-        </ListContainer>
-      </ListItem>
-
-      <ListItem>
-        <DiSass size="3rem" />
-        <ListContainer>
-          <ListParagraph>
-            Sass
-          </ListParagraph>
-        </ListContainer>
-      </ListItem>
-
-      <ListItem>
-        <DiScrum size="3rem" />
-        <ListContainer>
-          <ListParagraph>
-            Scrum
-          </ListParagraph>
-        </ListContainer>
-      </ListItem>
-
-      <ListItem>
-        <DiTrello size="3rem" />
-        <ListContainer>
-          <ListParagraph>
-            Trello
-          </ListParagraph>
-        </ListContainer>
-      </ListItem>
-
-      <ListItem>
-        <DiTrello size="3rem" />
-        <ListContainer>
-          <ListParagraph>
-            Trello
-          </ListParagraph>
-        </ListContainer>
-      </ListItem>
-
-      <ListItem>
-        <DiUbuntu size="3rem" />
-        <ListContainer>
-          <ListParagraph>
-            Ubuntu
-          </ListParagraph>
-        </ListContainer>
-      </ListItem>
-
-      <ListItem>
-        <DiWordpress size="3rem" />
-        <ListContainer>
-          <ListParagraph>
-            WordPress
-          </ListParagraph>
-        </ListContainer>
+        <ListTitle>Column Three</ListTitle>
+        <DiFirebase size="3rem" />
+          <ListContainer>
+            <ListParagraph>
+              One
+              <br />
+              Two
+              <br />
+              Three
+              <br />
+            </ListParagraph>
+          </ListContainer>
       </ListItem>
 
     </List>
