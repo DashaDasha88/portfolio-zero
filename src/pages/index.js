c096979e64b56ff98b208c6295a3efca52c101e2
Dasha <dasha.kotova.mail@gmail.com>
2021-06-26 18:09:50 -0400

    update references to about me page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,7 @@
-import Acomplishments from '../components/Acomplishments/Acomplishments';
-import BgAnimation from '../components/BackgrooundAnimation/BackgroundAnimation';
 import Hero from '../components/Hero/Hero';
 import Projects from '../components/Projects/Projects';
 import Technologies from '../components/Technologies/Technologies';
-import AbourMe from '../components/AboutMe/AboutMe';
+import AboutMe from '../components/AboutMe/AboutMe';
 import { Layout } from '../layout/Layout';
 import { Section } from '../styles/GlobalComponents';
 
